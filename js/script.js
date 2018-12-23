29cb0c6aa73c089890f2d7573b3e1d8025f890c0
GuyAgiv <guyagiv@gmail.com>
2018-12-23 19:00:45 +0200

    add js conditions to master

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log("tbd")
+//Text
+console.log("Hello World!");
+
+var populationArray = [2,5,3,4,7];
+var maxPopulation = Math.floor(Math.random()*10)*100000;
+var isOverPopulated = true; 
+
+for (var i=0;i<populationArray.length;i++){
+
+switch(maxPopulation){
+
+case maxPopulation>populationArray[i]:console.log("larger");
+case maxPopulation<populationArray[i]:console.log("smaller");
+case maxPopulation==populationArray[i]:console.log("equals");
+default:
+
+}
+}
+
+
+if((populationArray[1]+populationArray[2]+populationArray[3]<100000 && maxPopulation<100000) || !isOverPopulated ){
+
+    console.log("Small cities!!!")
+}
+else
+ console.log("Large cities!!!")
